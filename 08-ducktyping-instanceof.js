e5e4189a6b021c41f3d7d81e3a93d5f8297c2927
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-03-16 00:36:53 -0300

    updating, hasta aqui aun no hemos utililizado 'instanceof', y aun nose para que sirve jeje

--- 08-ducktyping-instanceof.js
+++ 08-ducktyping-instanceof.js
@@ -1,9 +1,39 @@
 // el ejemplo a seguir aunque la sintaxis sea diferente, es lo mismo 
-// solo lo puse en parentesis para diferenciar graficamente en el navegador
+// solo lo puse en parentesis para diferenciar graficamente en el editor,
 // que es un atributo private
 // se puede observar el ejemplo en los getters y setters de la funcion -createStudent-
 // (_name: name) == ("_name": name)
 //  ("_learningPaths": learningPaths) == _learningPaths: learningPaths
+
+
+
+// ------deepCopy--- init here 
+function typeOfElement (element){
+  switch (Object.prototype.toString.call(element)){
+    case "[object Object]":
+      return "object";
+    case "object Array ":
+      return "array";
+    default:
+      return "It's probably primitive data."
+  }
+}
+
+function deepCopy(element) {
+  let elementType = typeOfElement(element);
+  if(elementType !== "array" && elementType !== "object") return element;
+  
+  let copyOfElement;
+  if(elementType === "object") copyOfElement = {};
+  if(elementType === "array") copyOfElement = [];
+
+  for(let item in element){
+    copyOfElement[item] = deepCopy(element[item])
+  }
+
+  return copyOfElement;
+}
+// --------------end here---------------- 
 
 function requiredParam(param) {
   throw new Error(param + " es obligatorio");
@@ -35,9 +65,7 @@ function createLearningPath({
     return private._courses;
   },
  };
-
  return public;
-
 }
 
 function createStudent({
@@ -82,16 +110,21 @@ function createStudent({
       return private["_learningPaths"];
     },
     set learningPaths(newLP){
-      !newLP.name
-      if(newName.length != 0){
-        private._name = newName;
-      }else{
-        console.warn("Tu nombre deve tener al menos 1 caracter")
+      if(!newLP.name){
+        console.warn("Tu LP no tiene la propiedad 'name' ")
+        return;
       }
+      if(!newLP.courses){
+        console.warn("Tu LP no tiene la propiedad 'courses'")
+        return;
+      }
+      if(!Array.isArray(newLP.courses)){
+        console.warn("Tu LP no es una lista (* de cursos)");
+        return;
+      }
+      private["_learningPaths"].push(newLP);
     },
   };
-
-
   return public;
 }
 
